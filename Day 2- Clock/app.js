96932e8ff56dd3fb835c0aedec8f950785df0b3a
VaibhavVerma16113108 <52669625+VaibhavVerma16113108@users.noreply.github.com>
2020-08-04 12:40:04 +0530

    Add files via upload

---
+++ Day 2- Clock/app.js
@@ -0,0 +1,18 @@
+const secondHand = document.querySelector('#second');
+const minuteHand = document.querySelector('#minute');
+const hourHand = document.querySelector('#hour');
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const secondsDegrees = (seconds/60) * 360 + 90;
+    const minutesDegrees = (minutes/60) * 360 + 90;
+    const hoursDegrees = (hours/12) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(seconds);
+}
+
+setInterval(setDate, 1000);
